4f2ba46ef1ec0cba205850acfa777699074294aa
Tony Vongvone <tvongvone@gmail.com>
2022-12-19 22:57:38 -0800

    Adjusted user.js

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -27,9 +27,9 @@ const validateSignup = [
 ]
 
 router.post('/', validateSignup, async(req, res) => {
-    const {email, password, username} = req.body
+    const {firstName, lastName, email, password, username} = req.body
 
-    const user = await User.signup({ email, username, password })
+    const user = await User.signup({ firstName, lastName, email, username, password })
 
     await setTokenCookie(res, user);
 
