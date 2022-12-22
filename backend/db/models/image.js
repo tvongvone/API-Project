4a6d205b3b4e84ef268d810092a3a3967a9e8b15
Tony Vongvone <tvongvone@gmail.com>
2022-12-21 19:10:47 -0800

    finished image

---
+++ backend/db/models/image.js
@@ -0,0 +1,40 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Image extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+
+    static associate(models) {
+      // define association here
+      Image.belongsTo(models.Spot, {
+        foreignKey: 'spotId'
+      })
+    }
+  }
+  Image.init({
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    url: {
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+    preview: DataTypes.BOOLEAN
+  }, {
+    sequelize,
+    modelName: 'Image',
+    defaultScope: {
+      attributes: {
+        exclude: ['spotId', 'createdAt', 'updatedAt']
+      }
+    }
+  });
+  return Image;
+};
