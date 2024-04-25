cc1dd7343e7dd9f3f4bce830ece866319ac2de34
Tony Vongvone <tvongvone@gmail.com>
2024-04-24 22:56:29 -0700

    fix

--- frontend/src/components/CreateSpot/index.js
+++ frontend/src/components/CreateSpot/index.js
@@ -20,5 +20,9 @@ export default function CreateSpot() {
     const spot = {country: "", address: "", city: "", state: "", lat: "", lng: "", name: "", description: "", price: "", previewImage: ""}
     return spot ? (
         <FormPage spot={spot} formType='Create'/>
+<<<<<<< HEAD
     ): <div>This would be an error</div>
+=======
+    ): <div></div>
+>>>>>>> 89c8c1e36aad18cf0366a41de4400b033856b374
 }
