f88c36ae463c055a4b5d1ca0fa2e134027c51cc1
Tony Vongvone <tvongvone@gmail.com>
2024-08-01 19:20:29 -0700

    fix broken component

--- frontend/src/components/CreateSpot/index.js
+++ frontend/src/components/CreateSpot/index.js
@@ -20,13 +20,5 @@ export default function CreateSpot() {
     const spot = {country: "", address: "", city: "", state: "", lat: "", lng: "", name: "", description: "", price: "", previewImage: ""}
     return spot ? (
         <FormPage spot={spot} formType='Create'/>
-<<<<<<< HEAD
-<<<<<<< HEAD
     ): <div>This would be an error</div>
-=======
-    ): <div></div>
->>>>>>> 89c8c1e36aad18cf0366a41de4400b033856b374
-=======
-    ): <div></div>
->>>>>>> 89c8c1e36aad18cf0366a41de4400b033856b374
 }
